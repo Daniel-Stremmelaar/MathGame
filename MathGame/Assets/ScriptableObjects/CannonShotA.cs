36617cd289156cb99ec82637ca257071b8ea1c8b
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2020-01-07 10:00:02 +0100

    Work Start
    Started working, scriptable objects used for shots.

---
+++ MathGame/Assets/ScriptableObjects/CannonShotA.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ShotTypeA", menuName = "ScriptableObjects/SimpleShot", order = 1)]
+public class CannonShotA : ScriptableObject
+{
+    public int sulfur;
+    public int charcoal;
+}
